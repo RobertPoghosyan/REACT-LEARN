94cb2ef5a6d090819e49188d0760a967de343f79
RobertPoghosyan <[robertpoghosian@gmail.com]>
2021-01-10 13:28:56 +0400

    Homework N4-Layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import Form from './containers/Form/Form';
-
-
+import Layout from './components/Layout/Layout'
+import Header from './containers/Header/Header'
+import Footer from './containers/Footer/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Layout>
+        <main className="app-main-content">
+          <Form/>
+        </main>
+        <Footer />
+      </Layout>
       
-      <Form />
       
     </div>
   );
